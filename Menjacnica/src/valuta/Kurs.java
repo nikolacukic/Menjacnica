f59942588d50023867aa7141a41d4c17c1956178
nikolacukic <nikolacukic@gmail.com>
2017-03-28 19:51:29 +0200

    U klasi Valuta generisane metode toString, hashCode i equals, equals i toString redefinisane U klasi Kurs generisane metode toString, hashCode i equals, toString redefinisana

--- Menjacnica/src/valuta/Kurs.java
+++ Menjacnica/src/valuta/Kurs.java
@@ -32,4 +32,45 @@ public class Kurs {
 	public void setKupovni(double kupovni) {
 		this.kupovni = kupovni;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datumRacunanjaKursa == null) ? 0 : datumRacunanjaKursa.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datumRacunanjaKursa == null) {
+			if (other.datumRacunanjaKursa != null)
+				return false;
+		} else if (!datumRacunanjaKursa.equals(other.datumRacunanjaKursa))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kurs na dan: " + datumRacunanjaKursa + " Prodajni=" + prodajni + ", srednji=" + srednji
+				+ ", kupovni=" + kupovni;
+	}
 }
--- Menjacnica/src/valuta/Valuta.java
+++ Menjacnica/src/valuta/Valuta.java
@@ -24,4 +24,25 @@ public class Valuta {
 	public void setKursNaDan(LinkedList<Kurs> kursNaDan) {
 		this.kursNaDan = kursNaDan;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursNaDan == null) ? 0 : kursNaDan.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Valuta) {
+			Valuta valuta = (Valuta) obj;
+			if(valuta.getNaziv().equals(this.getNaziv()) && valuta.getSkraceniNaziv().equals(this.getSkraceniNaziv())) return true;
+		}
+		return false;
+	}
+	@Override
+	public String toString() {
+		return "Valuta: " + naziv + " " + skraceniNaziv;
+	}
 }
