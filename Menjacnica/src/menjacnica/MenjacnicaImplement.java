b3b7f13c2b933e2c93895154e1f5b2efd30c693e
nikolacukic <nikolacukic@gmail.com>
2017-04-01 21:38:43 +0200

    Uvedena klasa MenjacnicaImplement koja implementira MenjacnicaInterfejs, bez implementiranja metoda

---
+++ Menjacnica/src/menjacnica/MenjacnicaImplement.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+import valuta.Kurs;
+
+public class MenjacnicaImplement implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursNaDan(String valuta, Kurs kurs, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbrisiKursNaDan(String valuta, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs pronadjiKurs(String valuta, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
