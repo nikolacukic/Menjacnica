ae306677af1c3a784be09b4cffa3b526c46714f0
nikolacukic <nikolacukic@gmail.com>
2017-04-01 21:55:19 +0200

    Implementirane metode za dodavanje brisanje i nalazenje kursa na dan. Izmenjeni parametri koje metode primaju

--- Menjacnica/src/menjacnica/MenjacnicaImplement.java
+++ Menjacnica/src/menjacnica/MenjacnicaImplement.java
@@ -1,28 +1,62 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 import valuta.Kurs;
+import valuta.Valuta;
 
 public class MenjacnicaImplement implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> valute = new LinkedList<>();
+	
 	@Override
-	public void dodajKursNaDan(String valuta, Kurs kurs, GregorianCalendar dan) {
-		// TODO Auto-generated method stub
+	public void dodajKursNaDan(Valuta valuta, Kurs kurs, GregorianCalendar dan) {
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).equals(valuta)){
+				LinkedList<Kurs> kursevi = valute.get(i).getKursNaDan();
+				for (int j = 0; j < kursevi.size(); j++) {
+					if(kursevi.get(j).getDatumRacunanjaKursa().get(GregorianCalendar.DATE) == dan.get(GregorianCalendar.DATE)){
+						throw new RuntimeException("Postoji kurs za taj datum");
+			 	}
+			 		}
+			 	valute.get(i).getKursNaDan().add(kurs);
+			 	return;
+			 }
+		}
 		
 	}
 
 	@Override
-	public void izbrisiKursNaDan(String valuta, GregorianCalendar dan) {
-		// TODO Auto-generated method stub
-		
+	public void izbrisiKursNaDan(Valuta valuta, GregorianCalendar dan) {
+	
+		for (int i = 0; i < valute.size(); i++) {
+			if( valute.get(i).equals(valuta)){
+				LinkedList<Kurs> kursevi = valute.get(i).getKursNaDan();
+			 	for (int j = 0; j < kursevi.size(); j++) {
+			 		if(kursevi.get(j).getDatumRacunanjaKursa().get(GregorianCalendar.DATE) == dan.get(GregorianCalendar.DATE)){
+			 			valute.get(i).getKursNaDan().remove(j);
+			 			return;
+			 		}
+			 	}
+			 }
+		}
 	}
 
 	@Override
-	public Kurs pronadjiKurs(String valuta, GregorianCalendar dan) {
-		// TODO Auto-generated method stub
-		return null;
+	public Kurs pronadjiKurs(Valuta valuta, GregorianCalendar dan) {
+		for (int i = 0; i < valute.size(); i++) {
+			 if( valute.get(i).equals(valuta)){
+			 	LinkedList<Kurs> kursevi = valute.get(i).getKursNaDan();
+			 	for (int j = 0; j < kursevi.size(); j++) {
+			 		if(kursevi.get(j).getDatumRacunanjaKursa().get(GregorianCalendar.DATE) == dan.get(GregorianCalendar.DATE)){
+			 			return valute.get(i).getKursNaDan().get(j);
+			 		}
+			 	}
+			 }
+		}
+		throw new RuntimeException("Ne postoji kurs za taj datum");
 	}
 
 }
