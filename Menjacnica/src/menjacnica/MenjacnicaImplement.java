eed91c89744082eb53b516d9d4bda11856484254
nikolacukic <nikolacukic@gmail.com>
2017-04-01 22:00:12 +0200

    Implementirana metoda za dodavanje kursa na master grani

--- Menjacnica/src/menjacnica/MenjacnicaImplement.java
+++ Menjacnica/src/menjacnica/MenjacnicaImplement.java
@@ -1,15 +1,29 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 import valuta.Kurs;
+import valuta.Valuta;
 
 public class MenjacnicaImplement implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 	@Override
 	public void dodajKursNaDan(String valuta, Kurs kurs, GregorianCalendar dan) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			 if( valute.get(i).getNaziv().equals(valuta)){
+			 	LinkedList<Kurs> kursevi = valute.get(i).getKursNaDan();
+			 	for (int j = 0; j < kursevi.size(); j++) {
+			 		if(kursevi.get(j).getDatumRacunanjaKursa().get(GregorianCalendar.DATE) == dan.get(GregorianCalendar.DATE)){
+			 			throw new RuntimeException("Vec postoji kurs za taj datum");
+			 		}
+			 	}
+			 	valute.get(i).getKursNaDan().add(kurs);
+			 	return;
+			}
+		}
 		
 	}
 
